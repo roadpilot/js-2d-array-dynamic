871ad95793cc747d672042da270bc1036cefc996
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-19 21:29:13 +0000

    first attempt converting c++ solution to js

--- index.js
+++ index.js
@@ -2,3 +2,42 @@
 542. 01 Matrix
 Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
 */
+
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var updateMatrix = function(mat) {
+        let rows = mat.length;
+        if (rows == 0) 
+            return mat;
+        let cols = mat[0].length;
+        let dist = [...Array(cols)].map(e => Array(cols).fill([]));
+
+        //First pass: check for left and top
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                } else {
+                    if (i > 0)
+                        dist[i][j] = Math.min(dist[i][j], dist[i - 1][j] + 1);
+                    if (j > 0)
+                        dist[i][j] = Math.min(dist[i][j], dist[i][j - 1] + 1);
+                }
+            }
+        }
+
+        //Second pass: check for bottom and right
+        for (let i = rows - 1; i >= 0; i--) {
+            for (let j = cols - 1; j >= 0; j--) {
+                if (i < rows - 1)
+                    dist[i][j] = Math.min(dist[i][j], dist[i + 1][j] + 1);
+                if (j < cols - 1)
+                    dist[i][j] = Math.min(dist[i][j], dist[i][j + 1] + 1);
+            }
+        }
+        return dist;
+
+    
+};
