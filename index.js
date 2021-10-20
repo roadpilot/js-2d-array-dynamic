431d331c3602ba0e1c09ee5634561caf1ec31686
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-20 05:19:43 +0000

    refactor

--- index.js
+++ index.js
@@ -9,19 +9,23 @@ Given an m x n binary matrix mat, return the distance of the nearest 0 for each 
  */
 var updateMatrix = function(mat) {
   let rows = mat.length;
-  if (rows == 0) 
-      return mat;
   let cols = mat[0].length;
-  let dist = [...Array(cols)].map(e => Array(cols).fill([]));
+  // console.log(rows,cols)
+  if (rows === 0 || cols === 1) 
+      return mat;
+  let dist = [...Array(rows)].map(e => Array(cols).fill([]));
+  // console.log(dist)
 
   //First pass: check for left and top
   for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
-          if (mat[i][j] == 0) {
+          if (mat[i][j] === 0) {
+              console.log(i,j)
               dist[i][j] = 0;
           } else {
               top = i ? mat[i-1][j] : Infinity
               left = j ? mat[i][j-1] : Infinity
+              // console.log(top,left)
               dist[i][j] = Math.min(top, left) +1
           }   
       }
@@ -32,8 +36,8 @@ var updateMatrix = function(mat) {
       for (let j = cols - 1; j >= 0; j--) {
               bottom = i<rows-1 ? mat[i+1][j] : Infinity
               right = j<cols-1 ? mat[i][j+1] : Infinity
-              dist[i][j] = Math.min(mat[i][j], bottom +1, right +1)
+              dist[i][j] = Math.min(dist[i][j], bottom +1, right +1)
       }
   }
-  return dist;  
+  return dist;
 };
